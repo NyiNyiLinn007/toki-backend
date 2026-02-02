342c6bf6e5c3f6247d88974ae6dc77c4dfc2d81d
Nyi Nyi Linn <nn.l@mit.com.mm>
2026-02-02 21:14:31 +0630

    allow vercel.app

--- server.js
+++ server.js
@@ -90,6 +90,14 @@ app.use((req, res) => {
     });
 });
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://toki-message.vercel.app');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  if (req.method === 'OPTIONS') return res.sendStatus(204);
+  next();
+});
+
 // Error handler (must be last)
 app.use(errorHandler);
 
